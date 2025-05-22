45ea31f1814b0c1857f2d8cc44db3f2d9b254a6f
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-05-22 18:00:13 +0200

    Using more general parameters in IEFunction methods

--- InterEx/interfaceTypes/IEFunction.cs
+++ InterEx/interfaceTypes/IEFunction.cs
@@ -7,11 +7,11 @@ namespace InterEx.InterfaceTypes
 {
     public partial record class IEFunction(IEEngine Engine, IList<string> Parameters, Statement Root, Scope Scope)
     {
-        public Value InvokeRaw(Value[] arguments)
+        public Value InvokeRaw(IReadOnlyList<Value> arguments)
         {
-            if (this.Parameters.Count > arguments.Length)
+            if (this.Parameters.Count > arguments.Count)
             {
-                throw new IERuntimeException($"Argument type mismatch, expected {this.Parameters.Count}, but got {arguments.Length}");
+                throw new IERuntimeException($"Argument type mismatch, expected {this.Parameters.Count}, but got {arguments.Count}");
             }
 
             var innerScope = this.Scope.MakeChild();
@@ -23,17 +23,17 @@ namespace InterEx.InterfaceTypes
             return this.Engine.Evaluate(this.Root, innerScope);
         }
 
-        public T Invoke<T>(params object[] arguments)
+        public T Invoke<T>(params IEnumerable<object> arguments)
         {
             return this.Engine.Integration.ExportValue<T>(this.InvokeRaw(arguments.Select(this.Engine.Integration.ImportValue).ToArray()));
         }
 
-        public void Invoke(params object[] arguments)
+        public void Invoke(params IEnumerable<object> arguments)
         {
             this.InvokeRaw(arguments.Select(this.Engine.Integration.ImportValue).ToArray());
         }
 
-        public object InvokeAndExport(Type resultType, object[] arguments)
+        public object InvokeAndExport(Type resultType, IEnumerable<object> arguments)
         {
             return this.Engine.Integration.ExportValue(this.InvokeRaw(arguments.Select(this.Engine.Integration.ImportValue).ToArray()), resultType);
         }
