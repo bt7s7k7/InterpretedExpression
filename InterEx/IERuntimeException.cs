049e1a5e0426f7a315de2caab3033c0fae31839c
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-05-26 17:49:40 +0200

    IERuntimeException.ContainsNativeError

--- InterEx/IERuntimeException.cs
+++ InterEx/IERuntimeException.cs
@@ -10,6 +10,18 @@ namespace InterEx
         public IERuntimeException(string message, IERuntimeException inner) : base(message + "\n  " + inner.Message.Replace("\n", "\n  "), inner.InnerException) { }
         public IERuntimeException(string message, Exception inner) : base(message, inner) { }
 
+        public bool ContainsNativeError()
+        {
+            var inner = this.InnerException;
+
+            while (inner != null)
+            {
+                if (inner is not IERuntimeException or System.Reflection.TargetInvocationException) return true;
+            }
+
+            return false;
+        }
+
         public string FlattenMessage()
         {
             var result = new StringBuilder();
